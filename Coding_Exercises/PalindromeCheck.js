a1cb4de44a702e1af767996928ab9485e714bdb9
Sean Kelly <36159246+SeanKelly369@users.noreply.github.com>
2018-09-05 11:19:43 +0100

    Add files via upload

---
+++ Coding_Exercises/PalindromeCheck.js
@@ -0,0 +1,31 @@
+let str = 'murder redrum';
+PalindromeCheck(str);
+
+function PalindromeCheck(str) {
+  // Optional, but in this case made case insensitive
+  str = str.toLowerCase();
+
+  // Use a left, right and middle to reduce big O complexity by about half
+  let left = 0;
+  let right = str.length -1;
+  let middle = Math.floor(str.length / 2);
+
+  for(let i = 0; i < middle + 1; i++) {
+
+    if(str[left] === str[right]) {
+      left++;
+      right--;
+    }
+    // The iteration has reach the middle with matches on both sides
+    if(left === middle) {
+      return true;
+      break;
+    }
+
+    // A difference in character value is found
+    if(str[left] !== str[right]) {
+      return false;
+      break;
+    }
+  }
+}
