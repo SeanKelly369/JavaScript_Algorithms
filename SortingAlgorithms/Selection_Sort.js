39854e6ccdde44bc43f6d2f2cf34c0c64cf596dc
Sean Kelly <36159246+SeanKelly369@users.noreply.github.com>
2019-02-19 14:12:17 +0000

    Update Selection_Sort.js

--- SortingAlgorithms/Selection_Sort.js
+++ SortingAlgorithms/Selection_Sort.js
@@ -1,16 +1,19 @@
-function selectionSort(arr) {
-  for(let i = 0; i < arr.length; i++) {
-    let lowest = 1;
-    for(let j = (i+1); j < arr.length; j++) {
-      if( arr[j] < arr[lowest] ) {
-        lowest = j;
-      }
+function selectionSort(arr){
+    for(var i = 0; i < arr.length; i++){
+        var lowest = i;
+        for(var j = i+1; j < arr.length; j++){
+            if(arr[j] < arr[lowest]){
+                lowest = j;
+            }
+        }
+        if(i !== lowest){
+            //SWAP!
+            var temp = arr[i];
+            arr[i] = arr[lowest];
+            arr[lowest] = temp;
+        }
     }
-    let temp = arr[i];
-    arr[i] = arr[lowest];
-    arr[lowest] = temp;
-  }
-  return arr;
+    return arr;
 }
 
 function numberCompare(num1, num2) {
