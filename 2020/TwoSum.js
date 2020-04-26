17247411dbf805940116edf7fa4a204725d5ab3a
Sean Kelly <36159246+SeanKelly369@users.noreply.github.com>
2020-04-26 23:09:04 +0100

    Update TwoSum.js

--- 2020/TwoSum.js
+++ 2020/TwoSum.js
@@ -1,3 +1,5 @@
+// Approach using a HashMap
+
 // twoSum([2,7,11,15], 9);
 twoSum([45,2,1,6,9], 3);
 
@@ -12,3 +14,26 @@ function twoSum(nums, target) {
         map.set(nums[i], i);
     }
 };
+
+// Naive brute force N squared solution
+
+// Sample Inputs
+//let target = 6;
+//let nums = [3,2,4];
+
+function twoSum(nums, target) {
+    let tempArr = [];
+
+    for(let i = 0; i < nums.length; i++) {
+        
+        for(let j =0; j < nums.length; j++) {
+            
+            if( (i != j) && (nums[i] + nums[j] === target) ){
+
+                tempArr.push(i);
+                tempArr.push(j);
+                return tempArr;
+            }          
+        }     
+    }
+}
